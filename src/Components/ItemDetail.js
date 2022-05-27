ff62d7e66df7ad65f54d058ac424a8ea1d279036
Esteban Francisco Villalba <e.f.villalba@gmail.com>
2022-05-26 23:41:35 -0300

    Desafio Detalle Producto 01

--- src/Components/ItemDetail.js
+++ src/Components/ItemDetail.js
@@ -1,14 +1,27 @@
-const ItemDetail= ({ item }) => {
-return(
-<>
+const ItemDetail = ({ item }) => {
+    return (
+        <>
+            <div className="container content-fluid">
+                <div className="row  justify-content-center align-items-center">
 
-    <div>
-        <p>{item.description}</p>
+                    <div className="col " >
 
-    </div>
+                      
+                            <img src={item.image} class="rounded mx-auto  w-75 imgProducto" alt="..."></img>
+                      
+                    </div>
+                    <div className="col" >
+                        <h5 className="LetraBigBold">{item.nameProduct}</h5>
+                        <h5 className="LetraMedium">{item.description}</h5>
+                        <p className="LetraMediumBold mt-5">Precio ${item.cost}</p>
+                        <p className="LetraSmall">Stock {item.stock} unidades</p>
+                    </div>
 
-</>
-)
+
+                </div>
+            </div>
+        </>
+    )
 
 
 }
