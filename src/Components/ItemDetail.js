5cb095712bbb85636711760890c2a3a43e12c156
Esteban Francisco Villalba <e.f.villalba@gmail.com>
2022-05-29 23:46:15 -0300

    Primer Entrega Proyecto Final - Listo

--- src/Components/ItemDetail.js
+++ src/Components/ItemDetail.js
@@ -7,22 +7,22 @@ const ItemDetail = ({ item }) => {
         <>
         {
             item.stock > 0?
-            <div className="container  ">
-                <div className="row ">
+            <div className="container Detalle">
+                <div className="row vh-100 align-items-center">
 
                     <div className="col " >
-                            <img src={item.image} class="rounded " alt="..."></img>
+                            <img src={item.image} class="rounded w-75" alt="..."></img>
                       
                     </div>
-                {/*<div className="col " >
+                <div className="col text-center " >
                         <h5 className="LetraBigBold">{item.nameProduct}</h5>
-                        <h5 className="LetraMedium">{item.description}</h5>
+                          <h5 className="LetraMedium">{item.description}</h5>
                         <p className="LetraMediumBold mt-5">Precio ${item.cost}</p>
                         <p className="LetraSmall">Stock {item.stock} unidades</p>                        
                         <div>
                             <ItemCount stock={item.stock} initial={1} onAdd={onAdd} ></ItemCount>
                         </div>
-        </div>*/}
+                </div>
                     
 
 
